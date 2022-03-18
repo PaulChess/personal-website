162cf26ec5e163360920907da23e7681fa7e4e4c
paulChess <sjq15251852316@163.com>
2022-03-18 19:05:48 +0800

    chore: edit font-family & edit style

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -10,6 +10,17 @@ defineProps({
   },
 })
 const route = useRoute()
+
+onMounted(() => {
+  if (route.path.includes('/posts')) {
+    document.body.classList.remove('font-sans')
+    document.body.classList.add('font-mono')
+  }
+  else {
+    document.body.classList.remove('font-mono')
+    document.body.classList.add('font-sans')
+  }
+})
 </script>
 
 <template>
